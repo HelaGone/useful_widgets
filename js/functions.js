3f87f227c163238b4e1b9100e1bf58e2331966fb
helagone <luah613@gmail.com>
2018-01-11 16:11:38 -0600

    comparison widget

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,35 @@
 $(document).ready(function(){
+
 	console.log("functions here");
 
 	$("#defaultOpen").click();
-});
+
+    $(".twenty-container").twentytwenty();
+
+    $('.tab_display').on('click', function(){
+        var parent = $(this).parent(); 
+        parent.find('div.colapse_content').slideToggle("slow");
+    });
+
+    $('.nowadays_btn').on('click', function(){
+        console.log('click');
+        $(this).addClass('active');
+        $(this).parent().find('.comparison_btn').removeClass('active');
+
+        $(this).parent().parent().find('.figure_container1').hide();
+        $(this).parent().parent().find('.figure_container2').show();
+    });
+
+    $('.comparison_btn').on('click', function(){
+        console.log('click');
+        $(this).addClass('active');
+        $(this).parent().find('.nowadays_btn').removeClass('active');
+
+        $(this).parent().parent().find('.figure_container1').show();
+        $(this).parent().parent().find('.figure_container2').hide();
+    });
+
+});//END DOCUMENT READY
 
 function opentab(evt, tabName) {
     var i, tabcontent, tablinks;
